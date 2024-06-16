8dbe319ec9bc886fcc8b6ba019d366508ae4efe1
TANYA GOYAL <tanyagoyal15@TANYAs-MacBook-Air.local>
2024-06-16 23:26:24 +0530

    first commit

---
+++ app/update-prompt/page.jsx
@@ -0,0 +1,69 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import { useSession } from 'next-auth/react'
+import { useRouter, useSearchParams } from 'next/navigation'
+import Form from '@components/Form'
+
+const EditPrompt = () => {
+    const router = useRouter()
+    const searchParams = useSearchParams()
+    const promptId = searchParams.get('id')
+
+    const [submitting, setSubmitting] = useState(false)
+    const [post, setPost] = useState({
+        prompt: '',
+        tag: ''
+    })
+
+    useEffect(() => {
+        const getPromptDetails = async () => {
+            const response = await fetch(`/api/prompt/${promptId}`)
+            const data = await response.json()
+
+            setPost({
+                prompt: data.prompt,
+                tag: data.tag
+            })
+        }
+
+        if(promptId) getPromptDetails()
+    }, [promptId])
+
+    const updatePrompt = async (e) => {
+        e.preventDefault() // will prevent default behaviour of the browser i.e reload bcz we want least amount of reloads possible
+        setSubmitting(true)
+
+        if(!promptId) return alert('Prompt ID not found')
+
+        try {
+            const response = await fetch(`/api/prompt/${promptId}`, {
+                method: 'PATCH',
+                body: JSON.stringify({
+                    prompt: post.prompt,
+                    tag: post.tag
+                })
+            }) 
+
+            if(response.ok) {
+                router.push('/')
+            }
+        } catch (err) {
+            console.log(err)
+        } finally {
+            setSubmitting(false)
+        }
+    }
+
+  return (
+    <Form 
+        type='Create'
+        post={post}
+        setPost={setPost}
+        submitting={submitting}
+        handleSubmit={updatePrompt}
+    />
+  )
+}
+
+export default EditPrompt
